0449098d6c910c24b41e6276a03dbb1b81cbf343
Jon <22411356+jjhna@users.noreply.github.com>
2021-08-03 18:12:08 -0400

    Updated front page, header and footer
    Updated front page, header and footer
    
    Need to add projects, videos, blog and fix my notes
    
    Also need to fix color scheme if needed on the front page.
    
    Replaced images and text from the front page.

--- wp-content/themes/jonblog-theme/front-page.php
+++ wp-content/themes/jonblog-theme/front-page.php
@@ -1,19 +1,19 @@
 <?php get_header(); ?>
 
   <div class="page-banner">
-  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg') ?>);"></div>
+  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/titlepic.jpg') ?>);"></div>
     <div class="page-banner__content container t-center c-white">
       <h1 class="headline headline--large">Welcome!</h1>
-      <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
-      <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
-      <a href="<?php echo get_post_type_archive_link('program'); ?>" class="btn btn--large btn--blue">Find Your Major</a>
+      <h2 class="headline headline--medium">To my WordPress site</h2>
+      <!-- <h3 class="headline headline--small">Why don&rsquo;t you check out my previous <strong>projects</strong>?</h3> -->
+      <!-- <a href="<?php //echo get_post_type_archive_link('program'); ?>" class="btn btn--large btn--blue">Find Your Major</a> -->
     </div>
   </div>
 
   <div class="full-width-split group">
     <div class="full-width-split__one">
       <div class="full-width-split__inner">
-        <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
+        <h2 class="headline headline--small-plus t-center">Previous Projects</h2>
 
         <?php 
           $today = date('Ymd');
@@ -39,13 +39,13 @@
           }
         ?>
         
-        <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event') ?>" class="btn btn--blue">View All Events</a></p>
+        <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event') ?>" class="btn btn--blue">View All Projects</a></p>
 
       </div>
     </div>
     <div class="full-width-split__two">
       <div class="full-width-split__inner">
-        <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
+        <h2 class="headline headline--small-plus t-center">From My Blogs</h2>
         <?php
           $homepagePosts = new WP_Query(array(
             'posts_per_page' => 2
@@ -81,30 +81,30 @@
   <div class="hero-slider">
     <div data-glide-el="track" class="glide__track">
     <div class="glide__slides">
-    <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/bus.jpg'); ?>);">
+    <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/githubpic.jpeg'); ?>);">
     <div class="hero-slider__interior container">
       <div class="hero-slider__overlay">
-        <h2 class="headline headline--medium t-center">Free Transportation</h2>
-        <p class="t-center">All students have free unlimited bus fare.</p>
-        <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+        <h2 class="headline headline--medium t-center">View my Github page</h2>
+        <p class="t-center">View my github repositories</p>
+        <p class="t-center no-margin"><a href="https://jjhna.github.io/" class="btn btn--blue">Github page</a></p>
       </div>
     </div>
   </div>
-  <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/apples.jpg'); ?>);">
+  <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/portfoliopic.PNG'); ?>);">
     <div class="hero-slider__interior container">
       <div class="hero-slider__overlay">
-        <h2 class="headline headline--medium t-center">An Apple a Day</h2>
-        <p class="t-center">Our dentistry program recommends eating apples.</p>
-        <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+        <h2 class="headline headline--medium t-center">View my Portfolio page</h2>
+        <p class="t-center">View my portfolio: CV, projects and essays.</p>
+        <p class="t-center no-margin"><a href="https://github.com/jjhna" class="btn btn--blue">Portfolio page</a></p>
       </div>
     </div>
   </div>
-  <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/bread.jpg'); ?>);">
+  <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/linkedinpic.png'); ?>);">
     <div class="hero-slider__interior container">
       <div class="hero-slider__overlay">
-        <h2 class="headline headline--medium t-center">Free Food</h2>
-        <p class="t-center">Fictional University offers lunch plans for those in need.</p>
-        <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+        <h2 class="headline headline--medium t-center">View my LinkedIn page</h2>
+        <p class="t-center">Connect with me on LinkedIn!</p>
+        <p class="t-center no-margin"><a href="https://www.linkedin.com/in/jonna404/" class="btn btn--blue">LinkedIn page</a></p>
       </div>
     </div>
   </div>
--- wp-content/themes/jonblog-theme/header.php
+++ wp-content/themes/jonblog-theme/header.php
@@ -8,16 +8,15 @@
   <body <?php body_class(); ?>>
     <header class="site-header">
     <div class="container">
-      <h1 class="school-logo-text float-left"><a href="<?php echo site_url() ?>"><strong>Fictional</strong> University</a></h1>
+      <h1 class="school-logo-text float-left"><a href="<?php echo site_url() ?>"><strong>Jon</strong> Blog</a></h1>
       <a href="<?php echo esc_url(site_url('/search')); ?>" class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></a>
       <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
       <div class="site-header__menu group">
         <nav class="main-navigation">
           <ul>
-            <li <?php if (is_page('about-us') or wp_get_post_parent_id(0) == 16) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
-            <li <?php if (get_post_type() == 'program') echo 'class="current-menu-item"' ?>><a href="<?php echo get_post_type_archive_link('program') ?>">Programs</a></li>
-            <li <?php if (get_post_type() == 'event' OR is_page('past-events')) echo 'class="current-menu-item"';  ?>><a href="<?php echo get_post_type_archive_link('event'); ?>">Events</a></li>
-            <li <?php if (get_post_type() == 'campus') echo 'class="current-menu-item"' ?>><a href="<?php echo get_post_type_archive_link('campus'); ?>">Campuses</a></li>
+            <li <?php if (is_page('about-me') or wp_get_post_parent_id(0) == 16) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/about-me') ?>">About Me</a></li>
+            <li <?php if (get_post_type() == 'program') echo 'class="current-menu-item"' ?>><a href="<?php echo get_post_type_archive_link('program') ?>">Projects</a></li>
+            <li <?php if (get_post_type() == 'videos') echo 'class="current-menu-item"' ?>><a href="<?php echo get_post_type_archive_link('videos') ?>">Videos</a></li>
             <li <?php if (get_post_type() == 'post') echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/blog'); ?>">Blog</a></li>
           </ul>
         </nav>
